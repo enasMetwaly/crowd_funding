eae17e3ce136f4a82015c74a43889f2adf34f1c8
unknown <abdofaredd@gmail.com>
2023-10-22 23:50:01 +0300

    update models

--- mainproject/models.py
+++ mainproject/models.py
@@ -17,7 +17,7 @@ class Project(models.Model):
     title=models.CharField(max_length=100)
     details=models.CharField(max_length=100)
     total_target=models.DecimalField(max_digits=10,decimal_places=2)
-    creator=models.ForeignKey(User , on_delete=models.CASCADE, blank=True, null=True)
+    creator=models.ForeignKey(settings.AUTH_USER_MODEL , on_delete=models.CASCADE, blank=True, null=True)
     start_time=models.DateTimeField()
     end_time=models.DateTimeField()
     tags=models.ManyToManyField('Tag')
@@ -34,7 +34,7 @@ class Pictures(models.Model):
 
 class Comments(models.Model):
     project_name=models.ForeignKey(Project , on_delete=models.CASCADE, blank=True, null=True)
-    user_name=models.ForeignKey(User , on_delete=models.CASCADE, blank=True, null=True)
+    user_name=models.ForeignKey(settings.AUTH_USER_MODEL , on_delete=models.CASCADE, blank=True, null=True)
     text=models.TextField()
     replay=models.ForeignKey('self' , on_delete=models.CASCADE, blank=True, null=True)
 
@@ -45,4 +45,4 @@ class Reports(models.Model):
         ('comment', 'Comment'),
     )
     report_type=models.CharField(max_length=10,choices=REPORT_TYPES)
-    user_report=models.ForeignKey(User, on_delete=models.CASCADE, blank=True,null=True)
+    user_report=models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, blank=True,null=True)
