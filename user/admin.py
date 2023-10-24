421d6f19673386c01137fb24e53694c8029d3143
enasMetaly <enasmetwally99@gmail.com>
2023-10-24 09:29:41 +0300

    register user user profile in admin panel

--- user/admin.py
+++ user/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import User, UserProfile
+
+# Register your models here.
+admin.site.register(User)
+admin.site.register(UserProfile)
